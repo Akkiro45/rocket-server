6ae867433dc9a3ea5481ecf56ff86fa255e6b578
unknown <chudasamaakshay45@gmail.com>
2020-05-06 14:04:34 +0530

    Test

--- routes/link.js
+++ routes/link.js
@@ -1,95 +1,49 @@
 const express = require('express');
 const _ = require('lodash');
-const got = require('got');
-const metascraper = require('metascraper')([
-  require('metascraper-description')(),
-  require('metascraper-image')(),
-  require('metascraper-title')(),
-  require('metascraper-logo')(),
-  require('metascraper-url')()
-]);
-const scrape = require('scrape-metadata');
 const {getMetadata} = require('page-metadata-parser');
 const domino = require('domino');
 const fetch = require('isomorphic-fetch');
-const axios = require('axios');
-const http = require('http');
-const https = require('https');
+
 const Link = require('../models/link');
 const { authenticate } = require('../middlewares/authenticate');
 
 const router = express.Router();
 
-// router.post('/add', authenticate, async (req, res) => {
-//   let resBody = {};
-//   let error = {};
-//   const reqBody = _.pick(req.body, ['url', 'group']);
-//   try {
-//     if(reqBody.url) {
-//       const { body: html, url } = await got(reqBody.url);
-//       const metadata = await metascraper({ html, url });
-//       let body = {
-//         userId: req.user._id,
-//         url: metadata.url,
-//         title: metadata.title,
-//         description: metadata.description,
-//         image: metadata.image,
-//         logo: metadata.logo,
-//         createdAt: new Date().getTime()
-//       }
-//       if(reqBody.group) {
-//         body.group = reqBody.group
-//       }
-//       const link = new Link(body);
-//       const savedData = await link.save();
-//       resBody.status = 'ok';
-//       resBody.data = savedData;
-//       return res.status(200).send(resBody);
-//     } else {
-//       throw new Error('Error');
-//     }
-//   } catch(e) {
-//     error.msg = 'Invalid link!';
-//     resBody.status = 'error';
-//     resBody.error = error;
-//     return res.status(400).send(resBody);
-//   }
-// });
-
 router.post('/add', authenticate, async (req, res) => {
   let resBody = {};
   let error = {};
-  const reqBody = _.pick(req.body, ['url', 'group']);
+  const reqBody = _.pick(req.body, ['url', 'group', 'data']);
   try {
     if(reqBody.url) {
-      const config = { headers: {
-        'User-Agent': 'Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:63.0) Gecko/20100101 Firefox/63.0'
-      } }
-      const h = await axios(
-        {
-          method: 'GET',
-          httpAgent: new http.Agent({ keepAlive: true, maxSockets: 100 }),
-          httpsAgent: new https.Agent({ keepAlive: true, maxSockets: 100 }),
-          url: reqBody.url,
-          headers: config
+      let body;
+      if(reqBody.data) {
+        body = {
+          ...reqBody.data,
+          userId: req.user._id,
+          // url: reqBody.url,
+          // title: metadata.title,
+          // description: metadata.description,
+          // image: metadata.image,
+          // logo: metadata.icon,
+          createdAt: new Date().getTime()
         }
-      );
-      // console.log(h);
-      const response = await fetch(reqBody.url);
-      const html = await response.text();
-      const doc = domino.createWindow(h.data).document;
-      const metadata = getMetadata(doc, reqBody.url);
-      if(!metadata) {
-        throw new Error('Error');
-      }
-      let body = {
-        userId: req.user._id,
-        url: reqBody.url,
-        title: metadata.title,
-        description: metadata.description,
-        image: metadata.image,
-        logo: metadata.icon,
-        createdAt: new Date().getTime()
+      } else {
+        const response = await fetch(reqBody.url);
+        const html = await response.text();
+        const doc = domino.createWindow(html).document;
+        const metadata = getMetadata(doc, reqBody.url);
+        if(!metadata) {
+          throw new Error('Error');
+        }
+        body = {
+          userId: req.user._id,
+          url: reqBody.url,
+          title: metadata.title,
+          description: metadata.description,
+          image: metadata.image,
+          logo: metadata.icon,
+          createdAt: new Date().getTime()
+        }
       }
       if(reqBody.group) {
         body.group = reqBody.group
